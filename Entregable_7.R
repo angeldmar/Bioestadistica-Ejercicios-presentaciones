92367f98b0b2a3d9f98b394c7a07083f873d408d
Gilgamesh <angeldmartinezm2@outlook.com>
2024-05-28 13:46:16 -0600

    Avances en entragable 7

---
+++ Entregable_7.R
@@ -0,0 +1,57 @@
+library(readr)
+NeidertBD <- read_csv("Kluess-plasma DPPIV-data.csv")
+View(NeidertBD)
+
+# Preguntas de investigación
+# 1. ¿La relación observada gráficamente entre el IMC
+#(BMI Kg/m2) y el % de grasa (%Fat) es estadísticamente
+# significativa?
+
+str(NeidertBD)
+NeidertBD$Sex <- as.factor(NeidertBD$Sex)
+
+CorBMIFAT <- cor(NeidertBD$`BMI (kg/m2)`, NeidertBD$`% Fat`)
+CorBMIFAT
+
+# 0.4210956
+# correlacion moderada
+
+CorBMIFATtest <- cor.test(NeidertBD$`BMI (kg/m2)`, NeidertBD$`% Fat`)
+CorBMIFATtest 
+# valor p = 4.181e-06
+# 95 percent confidence interval:
+# 0.2546933 0.5632773
+# La correlacion es sestadisticamente significativa
+
+
+# 2.¿Cuán fuerte es la correlación entre (BMI Kg/m2)
+# y (%Fat)
+
+#coeficiente de Pearson
+# R
+#coeficiente de determinacion 
+# R^2
+
+R2BMIFAT <- CorBMIFAT^2
+R2BMIFAT
+# R^2 = 0.1773215
+# 17.73% de variabilidad en y es explicada por la variabilidad en x.
+
+
+
+# 3. Existe correlación entre (BMI Kg/m2) y (%Fat)?
+# Aplique la prueba t para correlación aplicando la
+# función cor.test() y haga la prueba de hipótesis:
+
+# creo que se aplica a la pregunta 1
+
+
+# 4. ¿Cuál es el IC95 de la correlación entre (BMI Kg/m2) y (%Fat)?
+
+# 95 percent confidence interval:
+# 0.2546933 0.5632773
+
+# 5. ¿Existe evidencia gráfica de relación entre las
+# variables (BMI Kg/m2) y (%Fat)?
+
+# A graficar. 
